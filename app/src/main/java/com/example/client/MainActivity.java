633f12f2ed57f2c9c3e3c62fa25acb30b65f5baf
QuXian <973162258@qq.com>
2018-04-25 01:15:23 +0800

    getip

--- app/src/main/java/com/example/client/MainActivity.java
+++ app/src/main/java/com/example/client/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.client;
 
 import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -13,6 +14,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketException;
 import java.net.SocketTimeoutException;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Enumeration;
 import java.util.List;
@@ -32,129 +34,139 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
 	Socket socket = null;
 	String buffer = "";
-	TextView txt1;
-	Button send;
-	EditText ed1;
-	EditText ed2;
-	String geted1;
-	String geted2;
 
-	public Handler myHandler = new Handler() {
-		@Override
-		public void handleMessage(Message msg) {
-			if (msg.what == 0x11) {
-				Bundle bundle = msg.getData();
-				txt1.append("反馈信息:"+bundle.getString("msg")+"\n");
-			}
-		}
+	Button wake;
+	Button ring;
+	Button show;
 
-	};
+	ArrayList<String> list = new ArrayList<>();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		txt1 = (TextView) findViewById(R.id.txt1);
-		send = (Button) findViewById(R.id.send);
-		ed1 = (EditText) findViewById(R.id.ed1);
-		ed2 = (EditText) findViewById(R.id.ed2);
 
-		send.setOnClickListener(new OnClickListener() {
+		wake = (Button) findViewById(R.id.btn_wake);//0
+		ring = (Button) findViewById(R.id.btn_ring);//1
+		show = (Button) findViewById(R.id.btn_show);//2
+
+
+		wake.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				geted2 = ed2.getText().toString();
-				if(geted2.equals("")){
-					txt1.append("请输入ip"+"\n");
-				}else {
-					geted1 = ed1.getText().toString();
-					txt1.append("发送信息:"+geted1+"\n");
-					//启动线程 向服务器发送和接收信息
-					new MyThread(geted1,geted2).start();
-				}
+				//启动线程 向服务器发送和接收信息
+				new MyThread("0").start();
+			}
+		});
+
+		ring.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				//启动线程 向服务器发送和接收信息
+				new MyThread("1").start();
+			}
+		});
+
+		show.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				//启动线程 向服务器发送和接收信息
+				new MyThread("2").start();
 			}
 		});
 	}
 
 	class MyThread extends Thread {
 
-		public ServerSocket serverSocket = null;
-
 		public String string;
-		public String IP;
 
-		public MyThread(String s,String ip) {
+		public MyThread(String s) {
 			string = s;
-			IP = ip;
 		}
 		//写入信息并获取反馈信息
 		@Override
 		public void run() {
+			list = getConnectedIP();
+			for(final String ip:list){
+				Log.e("ip",ip);
 
-			//定义消息
-			Message msg = new Message();
-			msg.what = 0x11;
-			Bundle bundle = new Bundle();
-			bundle.clear();
-
-			try {
-				Log.e("try","try");
-				//连接服务器 并设置连接超时为5秒
-				socket = new Socket();
-				socket.connect(new InetSocketAddress(IP, 30000), 1000);
-
-				BufferedReader bff = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-				//读取发来服务器信息
-				String line = null;
-				buffer="";
 				try {
-				while ((line = bff.readLine()) != null) {
-                    buffer = line + buffer;
-                }
-					OutputStream outputStream = socket.getOutputStream();
-					outputStream.write(string.getBytes("gbk"));
-					outputStream.flush();
+					Log.e("try","try");
+					//连接服务器 并设置连接超时为5秒
+					socket = new Socket();
+					socket.connect(new InetSocketAddress(ip, 30000), 1000);
 
-					bundle.putString("msg", buffer.toString());
-					msg.setData(bundle);
-					//发送消息 修改UI线程中的组件
-					myHandler.sendMessage(msg);
-					//关闭各种输入输出流
-				   bff.close();
-					outputStream.close();
-				}catch (IOException e) {
-					//连接超时 在UI界面显示消息
-					bundle.putString("msg", "连接失败!");
-					msg.setData(bundle);
-					//发送消息 修改UI线程中的组件
-					myHandler.sendMessage(msg);
+					BufferedReader bff = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+					//读取发来服务器信息
+					String line = null;
+					buffer="";
+					try {
+						while ((line = bff.readLine()) != null) {
+							buffer = line + buffer;
+						}
+						OutputStream outputStream = socket.getOutputStream();
+						outputStream.write(string.getBytes("gbk"));
+						outputStream.flush();
+
+						runOnUiThread(new Runnable() {
+							@Override
+							public void run() {
+								showToast("成功向"+ip+"传输："+buffer.toString());
+							}
+						});
+						//关闭各种输入输出流
+						bff.close();
+						outputStream.close();
+					}catch (IOException e) {
+						//连接超时 在UI界面显示消息
+						e.printStackTrace();
+					}
+				} catch (SocketTimeoutException aa) {
+					aa.printStackTrace();
+				} catch (IOException e) {
 					e.printStackTrace();
 				}
 
-			} catch (SocketTimeoutException aa) {
-				aa.printStackTrace();
-				bundle.putString("msg", "连接失败！");
-				msg.setData(bundle);
-				//发送消息 修改UI线程中的组件
-				myHandler.sendMessage(msg);
-			} catch (IOException e) {
-				e.printStackTrace();
-				bundle.putString("msg", "连接失败！");
-				msg.setData(bundle);
-				//发送消息 修改UI线程中的组件
-				myHandler.sendMessage(msg);
 			}
 		}
 	}
-	
+
+	private void showToast(String s) {
+		Toast.makeText(this,s,Toast.LENGTH_SHORT).show();
+	}
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	private ArrayList<String> getConnectedIP() {
+		ArrayList<String> connectedIP = new ArrayList<String>();
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(
+					"/proc/net/arp"));
+			String line;
+			while ((line = br.readLine()) != null) {
+				String[] splitted = line.split(" +");
+				if (splitted != null && splitted.length >= 4) {
+					String ip = splitted[0];
+					if(!ip.equals("IP")){
+						connectedIP.add(ip);
+					}
+
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return connectedIP;
 	}
 }
